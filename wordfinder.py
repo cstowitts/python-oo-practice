649c23ba021b7e4230e12e85225b948bdad1241f
hectorchu~ <hector.chu828@gmail.com>
2022-01-25 17:19:52 -0800

    Completed wordfinder prior to Code Review

--- wordfinder.py
+++ wordfinder.py
@@ -1,3 +1,5 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
@@ -8,12 +10,29 @@ class WordFinder:
         
     def populate_words_list (self, file_path):
         """populates words_list attribute with lines read from file, prints number of words read"""
-        file = open(file_path) #refactor using with open instead
-        for line in file:
-            self.words_list.append(line)
-        print(f"{len(self.words_list)} words read")
-        file.close()
+        with open(file_path) as file:
+            for line in file:
+                self.words_list.append(line.strip())
+            print(f"{len(self.words_list)} words read")
 
-    
+    def random (self):
+        """returns random word from words list"""
+        return choice(self.words_list)
 
-    def random (self, words_list):
+class SpecialWordFinder(WordFinder):
+    """Random Word Finder: finds random words"""
+    def __init__(self, file_path):
+        super().__init__(file_path)
+        self.words_list = []
+        self.special_populate_word_finder(file_path)
+
+    def special_populate_word_finder(self, file_path):
+        """populates word_list with lines read from file ignoring spaces, newlines, and comments"""
+        with open(file_path) as file:
+            for line in file:
+                if(line.startswith("#") or line.isspace()):
+                    pass
+                else:
+                    self.words_list.append(line.strip())
+            print(f"{len(self.words_list)} words read")
+
