36ec263921e1d345a51aff59be9b372d4263576c
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-01-25 16:13:12 -0800

    completes serial.py SerialGenerator class

--- serial.py
+++ serial.py
@@ -17,3 +17,19 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__ (self, start):
+        """create a SerialGenerator instance from start"""
+        self.start_num = start - 1
+        self.last_num = start - 1
+    
+    def generate (self):
+        """increments last_num by one and returns last_num"""
+        self.last_num += 1
+        return self.last_num 
+
+    def reset (self):
+        """resets last_num to start_num"""
+        self.last_num = self.start_num
+
+
