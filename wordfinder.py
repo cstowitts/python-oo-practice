d60509bf4260971e0eb76ab4b2140637eecfd087
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-01-25 16:34:53 -0800

    completes init and populate_words_list, starts random func

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,19 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__ (self, file_path):
+        """creates an instance of WordFinder, accepts file_path"""
+        self.words_list = []
+        self.populate_words_list(file_path)
+        
+    def populate_words_list (self, file_path):
+        """populates words_list attribute with lines read from file, prints number of words read"""
+        file = open(file_path) #refactor using with open instead
+        for line in file:
+            self.words_list.append(line)
+        print(f"{len(self.words_list)} words read")
+        file.close()
+
+    
+
+    def random (self, words_list):
